dc52960d6846ff7fb3d5b0b0c25541606f22c4ef
SOMNATH <somnathnikam2076@gmail.com>
2023-09-22 01:04:37 +0530

    1.0

--- app/src/main/java/com/example/notesapp/Database/Notedao.kt
+++ app/src/main/java/com/example/notesapp/Database/Notedao.kt
@@ -1,9 +1,28 @@
 package com.example.notesapp.Database
 
+import android.icu.text.CaseMap.Title
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.notesapp.Models.Note
 
 
 @Dao
 interface Notedao {
-    
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("Select * from notes_table order by id ASC")
+    fun getAllNotes():LiveData<List<Note>>
+
+
+    @Query("UPDATE notes_table SET title = :title, note= :note WHERE id = :id")
+    suspend fun update(id : Int?,title: String?,note: String?)
 }
